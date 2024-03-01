3c2315119b8f49a591add9750302535d61b2adea
Lokesh Bansal <lokesh@75way.com>
2024-03-01 18:50:41 +0530

    dashboard graph label changed and navbar icons changes

--- src/jsx/layouts/nav/AdminMenu.js
+++ src/jsx/layouts/nav/AdminMenu.js
@@ -1,49 +1,43 @@
 import { SVGICON } from "../../constant/theme";
 export const AdminMenuList = [
-    //Content
-    {
-        title: 'YOUR COMPANY',
-        classsChange: 'menu-title'
-    },
-    //Dashboard
-    {
-        title: 'Dashboard',
-        classsChange: 'mm-collapse',
-        iconStyle: SVGICON.Home,
-    },
-    {   title: 'Companay',
-       iconStyle: SVGICON.Employe,
-       to: '/company',
-   },
-    {   title: 'SubUser',
-       iconStyle: SVGICON.Employe,
-       to: '/subUser',
-    },
-    {
-        title: 'Driver',
-        iconStyle: SVGICON.Employe,
-        to: '/driver',
-     },
-    {
-        title: 'Vehicle',
-        iconStyle: SVGICON.Employe,
-        to: '/vehicle',
-     },
-    {
-        title: 'Tracking',
-        classsChange: 'mm-collapse',
-        iconStyle: SVGICON.Task,
-        content: [
-            {
-                title: 'Companies',
-                to: 'company-tracking',
-            },
-            {
-                title: 'Drivers',
-                to: 'driver-tracking',
-            },
-        ]
-    },
+  //Content
+  {
+    title: "YOUR COMPANY",
+    classsChange: "menu-title",
+  },
+  //Dashboard
+  {
+    title: "Dashboard",
+    classsChange: "mm-collapse",
+    iconStyle: SVGICON.Home,
+  },
+  { title: "Companay", iconStyle: SVGICON.Employe, to: "/company" },
+  { title: "SubUser", iconStyle: SVGICON.Employe, to: "/subUser" },
+  {
+    title: "Driver",
+    iconStyle: SVGICON.Employe,
+    to: "/driver",
+  },
+  {
+    title: "Vehicle",
+    iconStyle: SVGICON.Employe,
+    to: "/vehicle",
+  },
+  {
+    title: "Tracking",
+    classsChange: "mm-collapse",
+    iconStyle: SVGICON.Task,
+    content: [
+      {
+        title: "Companies",
+        to: "company-tracking",
+      },
+      {
+        title: "Drivers",
+        to: "driver-tracking",
+      },
+    ],
+  },
   {
     title: "Reports",
     iconStyle: SVGICON.Reports,
@@ -64,73 +58,73 @@ export const AdminMenuList = [
       },
     ],
   },
-    // {
-    //     title: 'Manage Clients',
-    //     iconStyle: SVGICON.ManageClient,
-    //     to: '/manage-client',
-    // },
-    {
-        title: 'Groups',
-        iconStyle: SVGICON.ManageClient,
-        to: '/groups',
-    },
-    {
-        title: 'Alerts',
-        iconStyle: SVGICON.ManageClient,
-        to: '/alert',
-    },
-    {
-        title: 'Expense',
-        iconStyle: SVGICON.ManageClient,
-        to: '/expense',
-    },
-    {
-        title: 'Geofence',
-        iconStyle: SVGICON.ManageClient,
-        to: '/geofence',
-    },
-    {
-        title: 'Classify Trips',
-        iconStyle: SVGICON.ManageClient,
-        to: '/classifyTrips',
-    },
-    {
-        title: 'Alerts',
-        iconStyle: SVGICON.ManageClient,
-        to: '/alert',
-    },
-    {
-        title: 'Expense',
-        iconStyle: SVGICON.ManageClient,
-        to: '/expense',
-    },
-    {
-        title: 'Geofence',
-        iconStyle: SVGICON.ManageClient,
-        to: '/geofence',
-    },
-    {
-        title: 'Classify Trips',
-        iconStyle: SVGICON.ManageClient,
-        to: '/classifyTrips',
-    },
-    {
-        title: 'Settings',
-        classsChange:'mm-collapse',
-        iconStyle: SVGICON.SettingMenu,
-        content:[
-            {
-                title:'General',
-                to: 'general'
-            },
-            {
-                title:'Master',
-                to: 'master'
-            },
-            {
-                title:'Technician',
-                to: 'technician'
-            }
-        ]
-    },
+  // {
+  //     title: 'Manage Clients',
+  //     iconStyle: SVGICON.ManageClient,
+  //     to: '/manage-client',
+  // },
+  {
+    title: "Groups",
+    iconStyle: SVGICON.ManageClient,
+    to: "/groups",
+  },
+  {
+    title: "Alerts",
+    iconStyle: SVGICON.ManageClient,
+    to: "/alert",
+  },
+  {
+    title: "Expense",
+    iconStyle: SVGICON.ManageClient,
+    to: "/expense",
+  },
+  {
+    title: "Geofence",
+    iconStyle: SVGICON.ManageClient,
+    to: "/geofence",
+  },
+  {
+    title: "Classify Trips",
+    iconStyle: SVGICON.ManageClient,
+    to: "/classifyTrips",
+  },
+  {
+    title: "Alerts",
+    iconStyle: SVGICON.ManageClient,
+    to: "/alert",
+  },
+  {
+    title: "Expense",
+    iconStyle: SVGICON.ManageClient,
+    to: "/expense",
+  },
+  {
+    title: "Geofence",
+    iconStyle: SVGICON.ManageClient,
+    to: "/geofence",
+  },
+  {
+    title: "Classify Trips",
+    iconStyle: SVGICON.ManageClient,
+    to: "/classifyTrips",
+  },
+  {
+    title: "Settings",
+    classsChange: "mm-collapse",
+    iconStyle: SVGICON.SettingMenu,
+    content: [
+      {
+        title: "General",
+        to: "general",
+      },
+      {
+        title: "Master",
+        to: "master",
+      },
+      {
+        title: "Technician",
+        to: "technician",
+      },
+    ],
+  },
 ];
