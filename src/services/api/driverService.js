c2e433165400aceb2126f41d81fe0ac69ba19114
Lokesh Bansal <lokesh@75way.com>
2024-03-29 19:38:40 +0530

    bug fix

--- src/services/api/driverService.js
+++ src/services/api/driverService.js
@@ -3,25 +3,25 @@ import initAxios from "./Axios";
 initAxios();
 
 export const createDriver = async (body) => {
-  return axios.post("/driver", body);
+  return axios.post("/drivers", body);
 };
 
 export const updateDriver = async (body) => {
-  return axios.patch("/driver", body);
+  return axios.patch("/drivers", body);
 };
 
 export const getDrivers = async (pageNo, limit) => {
-  const { data } = await axios.get(`/driver/?page=${pageNo}&limit=${limit}`);
+  const { data } = await axios.get(`/drivers/?page=${pageNo}&limit=${limit}`);
   return { data: data.data.data, totalLength: data.data.totalLength };
 };
 
 export const getDriverById = async (driverId) => {
-  const { data } = await axios.get(`/driver/${driverId}`);
+  const { data } = await axios.get(`/drivers/${driverId}`);
   return data.data;
 };
 
 export const deleteDrivers = async (body) => {
-  return axios.delete(`/driver`, {
+  return axios.delete(`/drivers`, {
     data: body,
   });
 };
