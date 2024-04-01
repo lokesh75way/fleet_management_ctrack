42da1f57734f49b6d53a7709dc4e67bef024b433
Lokesh Bansal <lokesh@75way.com>
2024-04-01 15:59:11 +0530

    bug fixed

--- src/services/api/Axios.js
+++ src/services/api/Axios.js
@@ -5,14 +5,8 @@ function initAxios() {
   axios.interceptors.request.use((config) => {
     const state = store.getState();
     const token = state.auth.auth.token;
-<<<<<<< Updated upstream
-    // config.baseURL = "https://8a0f-223-178-211-62.ngrok-free.app/api/fleet"
-    // config.baseURL = "http://192.168.1.29:5000/api/fleet/"
-    config.baseURL = process.env.REACT_APP_BACKEND_API
-=======
     // config.baseURL = process.env.REACT_APP_BACKEND_API
     config.baseURL = 'http://localhost:5000/api/fleet/'
->>>>>>> Stashed changes
     config.headers.Authorization = `Bearer ${token}`
     return config;
   });
