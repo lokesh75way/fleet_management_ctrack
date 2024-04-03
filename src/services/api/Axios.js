cebd4a926d9904d08dc7f6ae0524c763968ca3d6
Lokesh Bansal <lokesh@75way.com>
2024-04-03 20:21:37 +0530

    changes

--- src/services/api/Axios.js
+++ src/services/api/Axios.js
@@ -8,8 +8,8 @@ function initAxios() {
     config.baseURL = process.env.REACT_APP_BACKEND_API;
     // config.baseURL = "http://192.168.1.29:5000/api/fleet/";
     // config.baseURL = 'https://75way.com/api/fleet/'
-    // config.baseURL = 'http://localhost:5000/api/fleet/'
-    config.headers.Authorization = `Bearer ${token}`;
+    config.baseURL = 'http://localhost:5000/api/fleet/'
+    config.headers.Authorization = `Bearer ${token}`
     return config;
   });
 }
