07ce75566ac7c4db8339d0aed1fc437c44bdf3b9
Lokesh Bansal <lokesh@75way.com>
2024-04-09 18:12:40 +0530

    alert service

--- src/jsx/components/BranchDropdown/index.jsx
+++ src/jsx/components/BranchDropdown/index.jsx
@@ -4,44 +4,52 @@ import Select from "react-select";
 import usePagination from '../../../hooks/usePagination';
 
 const BranchDropdown = ({
-    onChange,
-    value,
-    customStyles,
-    name,
-    companyId,
-    isDisabled,
-    ref
+  onChange,
+  value,
+  customStyles,
+  name,
+  companyId,
+  isDisabled,
+  ref,
+  isMulti = true,
 }) => {
-    const [dropDownOptions, setdropDownOptions] = useState([]);
-    const [selectedOption, setSelectedOption] = useState(value);
-    const {page} = usePagination()
-    useEffect(() => {
-        const fetchBusinessGroups = async () => {
-            const response = await getAllBranch(undefined,companyId ? companyId : undefined);
-            const groupOptions = response.data.data.map(item => ({ value: item?._id, label: item?.branchName }));
-            console.log(response.data, "this is Branch data")
-            console.log(groupOptions, "this is Branch options")
-            setdropDownOptions(groupOptions);
-        };
-        fetchBusinessGroups();
-    }
-    , []);
-    useEffect(() => {
-        const selected = dropDownOptions.filter((option) => value && value.includes(option.value));
-        setSelectedOption(selected);
-    }, [value, dropDownOptions]);
+  const [dropDownOptions, setdropDownOptions] = useState([]);
+  const [selectedOption, setSelectedOption] = useState(value);
+  const { page } = usePagination();
+  useEffect(() => {
+    const fetchBusinessGroups = async () => {
+      const response = await getAllBranch(
+        undefined,
+        companyId ? companyId : undefined
+      );
+      const groupOptions = response.data.data.map((item) => ({
+        value: item?._id,
+        label: item?.branchName,
+      }));
+      console.log(response.data, "this is Branch data");
+      console.log(groupOptions, "this is Branch options");
+      setdropDownOptions(groupOptions);
+    };
+    fetchBusinessGroups();
+  }, []);
+  useEffect(() => {
+    const selected = dropDownOptions.filter(
+      (option) => value && value.includes(option.value)
+    )?.[0];
+    setSelectedOption(selected);
+  }, [value, dropDownOptions]);
 
-    return (
-            <Select
-                options = {dropDownOptions}
-                value={selectedOption}
-                onChange={(newValue) => onChange(newValue)}
-                styles={customStyles}
-                name={name}
-                ref={ref}
-                isDisabled={isDisabled || !companyId}
-                isMulti
-                />         
-    );
-}
+  return (
+    <Select
+      options={dropDownOptions}
+      value={selectedOption}
+      onChange={(newValue) => onChange(newValue)}
+      styles={customStyles}
+      name={name}
+      ref={ref}
+      isDisabled={isDisabled}
+      isMulti={isMulti}
+    />
+  );
+};
 export default BranchDropdown;
--- src/jsx/components/Tables/AlertTable.jsx
+++ src/jsx/components/Tables/AlertTable.jsx
@@ -1,40 +1,60 @@
-import React, { useEffect } from 'react'
+import React from "react";
 import { MdDelete } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
-import DeleteModal from '../Modal/DeleteModal';
+import DeleteModal from "../Modal/DeleteModal";
 
-const AlertTable = ({tableData, onConfirmDelete, editDrawerOpen}) => {
-    return (
-        <>
-            {tableData.map((item, index) => (
-                <tr key={index}>
-                    <td><span>{item.id}</span></td>
-                    <td>
-                        <div className="products">
-                                <h6>{item.name}</h6>
-                        </div>
-                    </td>
-                    <td><span>{item.alertType}</span></td>
-                    <td>
-                        <span>{item.createdDate}</span>
-                    </td>
-                    <td>
-                        <span>{item.notification}</span>
-                    </td>
-                    <td>
-                        <span>{item.reason}</span>
-                    </td>
-                    <td>
-                        <span className='d-flex justify-content-center'>
-                            <span className='cursor-pointer' ><FaEdit style={{ color: "green", fontSize: "1.2rem" }} /></span>
-                            <DeleteModal className='cursor-pointer ' onConfirmDelete={onConfirmDelete} id={item.id} ><MdDelete style={{ color: "red", fontSize: "1.2rem" }} /></DeleteModal>
-
-                        </span>
-                    </td>
-                </tr>
-            ))}
-        </>
-    )
-}
+const AlertTable = ({ tableData, onConfirmDelete, editDrawerOpen }) => {
+  return (
+    <>
+      {tableData.map((item, index) => (
+        <tr key={index}>
+          <td>
+            <span>{item.basedOn}</span>
+          </td>
+          <td>
+            <div className="products">
+              <h6>{item.alertName}</h6>
+            </div>
+          </td>
+          <td>
+            <span>{item.alertType}</span>
+          </td>
+          <td>
+            <span>
+              {new Date(item.createdAt).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "numeric",
+              })}
+            </span>
+          </td>
+          <td>
+            <span>{item.notification}</span>
+          </td>
+          <td>
+            <span>{item.reason}</span>
+          </td>
+          <td>
+            <span className="d-flex justify-content-center">
+              <span
+                className="cursor-pointer"
+                onClick={() => editDrawerOpen(item?._id)}
+              >
+                <FaEdit style={{ color: "green", fontSize: "1.2rem" }} />
+              </span>
+              <DeleteModal
+                className="cursor-pointer "
+                onConfirmDelete={onConfirmDelete}
+                id={item._id}
+              >
+                <MdDelete style={{ color: "red", fontSize: "1.2rem" }} />
+              </DeleteModal>
+            </span>
+          </td>
+        </tr>
+      ))}
+    </>
+  );
+};
 // onClick={() => editDrawerOpen(item.id)}
-export default AlertTable
+export default AlertTable;
