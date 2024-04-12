52f279d7d7d0b76bc961258ba21011759e48bb46
Lokesh Bansal <lokesh@75way.com>
2024-04-12 18:54:30 +0530

    geofence edit bug fix

--- src/jsx/components/maps/EditControl.jsx
+++ src/jsx/components/maps/EditControl.jsx
@@ -1,27 +1,18 @@
 import L from 'react-leaflet-draw'
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import {
-  FeatureGroup,
-  Circle,
-  Polygon,
-  Polyline,
-  Marker,
-  GeoJSON,
-} from "react-leaflet";
+import React, { useEffect, useRef, useState } from "react";
+import { FeatureGroup, Circle, Polygon, Polyline, Marker } from "react-leaflet";
 import { EditControl } from "react-leaflet-draw";
 import { useParams } from "react-router-dom";
 
-export default function EditControlFC({
-  points,
-  polygon,
-  lineString,
-  circles,
-  defaultValues,
-  setValues,
-}) {
+export default function EditControlFC({ watch, setValues, getValues }) {
   const ref = useRef(null);
   const { id } = useParams();
-  const [mapLayer, setmapLayer] = useState([]);
+  const [circles, setCircles] = useState([]);
+  const [lineString, setLineString] = useState([]);
+  const [polygon, setPolygon] = useState([]);
+  const [points, setPoints] = useState([]);
+  const watchedValues = watch("location");
+
   // const [polygonVertices, setPolygonVertices] = useState([]);
 
   // const poly = [
@@ -53,128 +44,123 @@ export default function EditControlFC({
   //   },
   // ];
 
-  useEffect(() => {
-    if (id && defaultValues?.location) {
-      setmapLayer([...defaultValues?.location]);
-    }
-  }, [defaultValues]);
+  const setShapes = () => {
+    const values = getValues("location");
+    setCircles([]);
+    setPoints([]);
+    setPolygon([]);
+    setLineString([]);
+    values?.map((loc) => {
+      if (loc.type === "Circle") {
+        setCircles((prev) => [...prev, loc]);
+      }
+      if (loc.type === "Polygon") {
+        setPolygon((prev) => [...prev, loc]);
+      }
+
+      if (loc.type === "Point") {
+        setPoints((prev) => [...prev, loc]);
+      }
+
+      if (loc.type === "LineString") {
+        setLineString((prev) => [...prev, loc]);
+      }
+    });
+  };
 
   const editChange = (e) => {
-    const {
-      layers: { _layers },
-    } = e;
-    Object.values(_layers).map(({ _leaflet_id, editing }) => {
-      // console.log(_leaflet_id);
-      setmapLayer((layers) =>
-        mapLayer.map((l) => {
-          if (_leaflet_id !== l.id) {
-            return l;
-          }
-          if (l && l.type === "Polygon") {
-            const coordinates = editing?.latlngs[0][0].map((coords) => {
-              return [coords.lat, coords.lng];
-            });
-            return l.id === _leaflet_id ? { ...l, coordinates } : l;
-          }
-          if (l && l.type === "Circle") {
-            const coordinates = [
-              editing._shape._latlng.lat,
-              editing._shape._latlng.lng,
-            ];
-            const duration = editing._shape._mRadius;
-            return l.id === _leaflet_id ? { ...l, coordinates, duration } : l;
-          }
-          if (l && l.type === "LineString") {
-            const coordinates = editing?.latlngs[0].map((coords) => {
-              return [coords.lat, coords.lng];
-            });
-            return l.id === _leaflet_id ? { ...l, coordinates } : l;
-          }
-          if (l && l.type === "Point") {
-            const coordinates = [
-              editing._marker._latlng.lat,
-              editing._marker._latlng.lng,
-            ];
-            return l.id === _leaflet_id ? { ...l, coordinates } : l;
-          }
-        })
-      );
+    let mapLayer = getValues("location") ?? [];
+    e.layers.eachLayer(({ editing, options }) => {
+      mapLayer = mapLayer.map((l) => {
+        if (options.id !== l.id) {
+          return l;
+        }
+        if (l && l.type === "Polygon") {
+          const coordinates = editing?.latlngs[0][0].map((coords) => {
+            return [coords.lat, coords.lng];
+          });
+          return l.id === options.id ? { ...l, coordinates } : l;
+        }
+        if (l && l.type === "Circle") {
+          const coordinates = [
+            editing._shape._latlng.lat,
+            editing._shape._latlng.lng,
+          ];
+          const duration = editing._shape._mRadius;
+          return l.id === options.id ? { ...l, coordinates, duration } : l;
+        }
+        if (l && l.type === "LineString") {
+          const coordinates = editing?.latlngs[0].map((coords) => {
+            return [coords.lat, coords.lng];
+          });
+          return l.id === options.id ? { ...l, coordinates } : l;
+        }
+        if (l && l.type === "Point") {
+          const coordinates = [
+            editing._marker._latlng.lat,
+            editing._marker._latlng.lng,
+          ];
+          return l.id === options.id ? { ...l, coordinates } : l;
+        }
+      });
+      setValues("location", mapLayer);
     });
   };
 
   const deleteHandle = (e) => {
-    const {
-      layers: { _layers },
-    } = e;
-    Object.values(_layers).map((_leaflet_id) => {
-      setmapLayer((layer) =>
-        layer.filter((l) => l.id !== _leaflet_id._leaflet_id)
-      );
+    let mapLayer = getValues("location") ?? [];
+    e.layers.eachLayer((ly) => {
+      mapLayer = mapLayer.filter((l) => l.id !== ly.options.id);
     });
+    setValues("location", mapLayer);
   };
 
   const onCreatedhandler = (e) => {
     const { layerType, layer } = e;
     const { _latlng } = layer;
+    let data = {};
     if (layerType === "circle") {
-      setmapLayer((lay) => {
-        return [
-          ...lay,
-          {
-            id: layer._leaflet_id,
-            type: "Circle",
-            coordinates: [_latlng.lat, _latlng.lng],
-            duration: layer._mRadius,
-          },
-        ];
-      });
+      data = {
+        id: layer._leaflet_id,
+        type: "Circle",
+        coordinates: [_latlng.lat, _latlng.lng],
+        duration: layer._mRadius,
+      };
     } else if (layerType === "polygon") {
       const coordinates = layer._latlngs[0].map((coords) => [
         coords.lat,
         coords.lng,
       ]);
-      setmapLayer((lay) => [
-        ...lay,
-        {
-          id: layer._leaflet_id,
-          type: "Polygon",
-          coordinates: coordinates,
-        },
-      ]);
+      data = {
+        id: layer._leaflet_id,
+        type: "Polygon",
+        coordinates: coordinates,
+      };
     } else if (layerType === "marker") {
-      setmapLayer((lay) => [
-        ...points,
-        ...lay,
-        {
-          type: "Point",
-          id: layer._leaflet_id,
-          coordinates: [layer._latlng.lat, layer._latlng.lng],
-        },
-      ]);
+      data = {
+        type: "Point",
+        id: layer._leaflet_id,
+        coordinates: [layer._latlng.lat, layer._latlng.lng],
+      };
     } else if (layerType === "polyline") {
       const coordinates = layer._latlngs.map((coords) => [
         coords.lat,
         coords.lng,
       ]);
-      setmapLayer((lay) => [
-        ...lay,
-        {
-          id: layer._leaflet_id,
-          type: "LineString",
-          coordinates: coordinates,
-        },
-      ]);
+      data = {
+        id: layer._leaflet_id,
+        type: "LineString",
+        coordinates: coordinates,
+      };
     }
+    const values = getValues("location") ?? [];
+    setValues("location", [...values, data]);
   };
 
   useEffect(() => {
-    if (mapLayer.length === 0) {
-      setValues("location", null);
-      return;
-    }
+    setShapes();
+  }, [watchedValues]);
 
-    setValues("location", mapLayer);
-  }, [mapLayer]);
   return (
     <FeatureGroup ref={ref}>
       {circles?.map((circle, index) => (
@@ -182,18 +168,25 @@ export default function EditControlFC({
           key={circle.id}
           center={circle.coordinates}
           radius={circle?.duration}
+          id={circle.id}
         ></Circle>
       ))}
       {points?.map((point, index) => {
-        return <Marker key={index} position={point.coordinates}></Marker>;
+        return (
+          <Marker
+            id={point.id}
+            key={point.id}
+            position={point.coordinates}
+          ></Marker>
+        );
       })}
 
       {polygon?.map((poly, index) => (
         <Polygon
+          id={poly.id}
           key={poly.id}
           pathOptions={{ color: "red" }}
           positions={poly.coordinates}
-          
         ></Polygon>
       ))}
 
@@ -202,12 +195,12 @@ export default function EditControlFC({
           key={line.id}
           positions={line.coordinates}
           path={line.coordinates}
+          id={line.id}
         ></Polyline>
       ))}
 
       {/* <Circle center={[25.2233, 55.2869]} radius={1000} /> */}
       <EditControl
-        key={mapLayer.length}
         position="topright"
         onEdited={editChange}
         onCreated={onCreatedhandler}
